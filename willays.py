bbaaee773cac5ca54f16c181a671e6ee80f36605
kassemhachi <kassem.hachi@outlook.com>
2022-07-08 01:20:45 +0100

    Grabbing communes and save it as csv file

---
+++ willays.py
@@ -0,0 +1,45 @@
+from msilib import Table
+from operator import index
+import re
+from bs4 import BeautifulSoup
+from numpy import number
+import requests
+import time
+import pandas as pd
+
+Commune_Info = {"NumCommune": [], "CommuneAr": [],
+                "Fax": [], "CommuneFr": [], "Willaya": []}
+
+
+def GrabbingData(url):
+    global df
+    global Commune_Info
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content, "html.parser")
+    contenttable = soup.find("div", {"class": "p-body-content"})
+    blocklist = contenttable.find_all("div", {"class": "block"})
+    body = blocklist[1].find("div", {"class": "block-body"})
+    Table = body.find("table", {"class": "dataList-table"})
+    tr = Table.find_all("tr")
+    listtr = list(tr)
+    del listtr[0]
+    for i in listtr:
+        td = i.find_all("td")
+        NumCommune = td[0].text
+        CommuneAr = td[1].text
+        CommuneFr = td[2].text
+        Fax = td[3].text
+        Willaya = td[4].text
+        Commune_Info["NumCommune"].append(NumCommune)
+        Commune_Info["CommuneAr"].append(CommuneAr)
+        Commune_Info["CommuneFr"].append(CommuneFr)
+        Commune_Info["Fax"].append(Fax)
+        Commune_Info["Willaya"].append(Willaya)
+
+
+for i in range(42):
+    url = "https://apcsali-adrar.dz/communes/?page="+str(i+1)
+    GrabbingData(url)
+    print(Commune_Info)
+df = pd.DataFrame(Commune_Info)
+df.to_excel('Communes.xlsx')
